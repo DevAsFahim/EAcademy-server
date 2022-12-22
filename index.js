61e891abff474dd25ab87b77f7a8b5899bce5dc1
ASFahim007 <devasfahim@gmail.com>
2022-12-22 20:03:15 +0600

    necessary commits

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const courses = require('./data/courses.json');
 
 app.use(cors())
 
+// default page
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// get course data
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
+// get category
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(c => c._id === id)
     res.send(selectedCourse)
 })
 
+// listed port
 app.listen(port, () => {
     console.log(`my app listening on port ${port}`)
 })
